cb84ce371943b0fe3a8ed775494322f5149dc7db
vitaliy <vitaliy-eroshin@yandex.ru>
2022-05-17 01:11:33 +0300

    Added image painting

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import requests
 import time
+import numpy as np
 
 session = requests.Session()
 token = ''
 address = 'http://localhost:8888/'
-delay = 1
+delay = 0.21
+
 
 def signin(login, password):
     request = {
@@ -38,12 +40,34 @@ def paintCell(x, y, color):
         print("Something went wrong when coloring ({x}, {y}) in {color}".format(x=x, y=y, color=color))
 
 
+def rgbToHex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+
+def paintImage(x, y, image):
+    for dy in range(len(image)):
+        for dx in range(len(image[0])):
+            print(dy * len(image[0]) + dx, '/', len(image) * len(image[0]))
+            color = image[dy][dx]
+            color = rgbToHex(tuple(color))
+            time.sleep(delay)
+            paintCell(x + dx, y + dy, color)
+
+
+def loadImage(path):
+    return np.array(Image.open(path))
+
+
 def __main__():
     if not signin(input("Enter your login: "), input("Enter your password: ")):
         return
 
-    time.sleep(delay)
+    image = loadImage(input("Enter path to the image: "))
+    paintImage(
+        int(input("Enter x (horizontal) coordinate of starting point: ")),
+        int(input("Enter y (vertical) coordinate of starting point: ")),
+        image
+    )
 
-    paintCell(3, 3, '#555555')
 
 __main__()
